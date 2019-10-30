e0ff2ee49196d9afd9f5b1bc7e0710d7d9c54f9a
DexterSyou <shotiti12@sina.com>
2019-10-30 22:38:33 +0900

    c

--- 002C/01Demo/0001Demo.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-
-  printf("%d \n", 1L<1U );
-  printf("%d \n", -1L>1UL);
-  return 0;
-}
--- 002C/01Demo/0002Demo.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-
-  printf("%d \n",'1'-'0');
-  return 0;
-}
