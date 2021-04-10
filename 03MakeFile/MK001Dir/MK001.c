099a0d7620d8bfd71c0d961f595c95d26f3fada1
DexterSyou <shotiti12@sina.com>
2021-04-10 10:13:21 +0800

    upd

--- 03MakeFile/MK001Dir/MK001.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "MK001.h"
-
-int main (void){
-
- printf("The number is %d \n" , MK001);
-}
-
--- 03MakeFile/MK002Dir/MK002.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "MK002.h"
-
-int main(void){
- 
-  printf("This is the second MakeFile Demo!!!! \n");
-}
