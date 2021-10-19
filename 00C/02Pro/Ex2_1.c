17692ce55aa7b77a22156e043ac92796a40b9c2b
DexterSyou <shotiti12@sina.com>
2021-10-19 16:53:57 +0800

    add

---
+++ 00C/02Pro/Ex2_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+ printf("%d\n",CHAR_MIN);
+ printf("%d\n",CHAR_MAX);
+ printf("%d\n",UCHAR_MAX);
+
+ printf("%d\n",INT_MIN);
+ printf("%d\n",INT_MAX);
+ printf("%ld\n",LONG_MIN);
+ printf("%ld\n",LONG_MAX);
+ printf("%f\n",DBL_MIN);
+ printf("%f\n",DBL_MAX);
+
+
+ return 0;
+}
---
+++ 05Shell/02Pro/018Ary.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# array variable to function test
+# adding values in an array
+
+function addarray {
+   local sum=0
+   local newarray
+   newarray=($(echo "$@"))
+   for value in ${newarray[*]}
+   do
+	   sum=$[ $sum + $value ]
+   done
+   echo $sum
+}
+
+myarray=(1 2 3 4 5)
+echo "The original array is ${myarray[*]}"
+arg1=$(echo ${myarray[*]})
+echo $arg1
+result=$(addarray $arg1)
+echo "The result is $result"
+
+
+
+
