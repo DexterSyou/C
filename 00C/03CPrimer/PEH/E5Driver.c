f07852a9f1dbdbca89607aa2fb45564da11949c5
DexterSyou <shotiti12@sina.com>
2022-02-22 15:20:40 +0800

    Create E5Driver.c

---
+++ 00C/03CPrimer/PEH/E5Driver.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "E5.h"
+
+
+#define SIZE 20
+
+int main(void){
+
+    Stack strline;
+    InitializeStack(&strline);
+
+    char str[SIZE];
+    printf("Please enter an string: \n");
+    scanf("%s",str);
+    printf("Your string is [ %s ] \n",str); 
+    int idx=0;
+    Item pushItem;
+    while(str[idx]!='\0'){
+        pushItem.ch = str[idx];
+        if(!FullStack(&strline)){
+            Push(pushItem, &strline);  
+        }else{
+            printf("Stack is full... \n");
+        }
+        idx++;
+    }    
+    Item popItem;
+    while(true){
+        if(!EmptyStack(&strline)){
+            Pop(&popItem,&strline);
+            printf("Pop a item : %c \n", popItem.ch);
+        }else{
+            printf("Stack is empty, has no item.\n");
+            break;
+        }
+    }
+
+
+    return 0;
+}
