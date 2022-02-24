f8553690479d2c341d148208dab525b9941eb0d2
DexterSyou <shotiti12@sina.com>
2022-02-24 09:25:47 +0800

    Create E1Driver.java

---
+++ 30Java/03TJ/PE7/E1Driver.java
@@ -0,0 +1,15 @@
+public class E1Driver{
+    private E1 e1;
+    public static void main(String[] args){
+        
+        if(e1==null){
+            System.out.println("e1 initing...");
+            e1 = new E1();
+        }
+
+    }
+}
+//
+class E1{
+
+}
