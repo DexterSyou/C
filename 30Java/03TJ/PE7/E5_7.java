9e6cb51a1c346cb7570e2781f040305326a09fe5
DexterSyou <shotiti12@sina.com>
2022-02-24 10:12:02 +0800

    Update E5_7.java

--- 30Java/03TJ/PE7/E5_7.java
+++ 30Java/03TJ/PE7/E5_7.java
@@ -1,21 +1,17 @@
 public class E5_7{
-
    public static void main(String[] args){
       new C();
       new C(90);
    }
-
 }
 
 class A{
   A(){
       System.out.println("A(); ");
   }
-
   A(int i){
      System.out.println("A(); "+i);
   }
-
 }
 
 class B{
@@ -31,13 +27,12 @@ class B{
 class C extends A{
     B b=new B();
     B b90=new B(90);
-
     C(){
         System.out.println("C(); ");
     }
+   
     C(int i){
         super(i);
         System.out.println("C(); "+i);
-
     }
 }
