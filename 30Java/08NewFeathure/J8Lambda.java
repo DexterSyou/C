cb4a7066aabf0bfc4ff4e526c9dc4be0f6145cad
DexterSyou <shotiti12@sina.com>
2022-04-25 10:33:30 +0800

    Update J8Lambda.java

--- 30Java/08NewFeathure/J8Lambda.java
+++ 30Java/08NewFeathure/J8Lambda.java
@@ -2,7 +2,11 @@ import java.util.*;
 public class J8Lambda {}
 //********************************* */
 /*函数式接口
-    只包含一个抽象方法的接口，称为函数式接口。*/
+    只包含一个抽象方法的接口，称为函数式接口。
+    可以通过 Lambda 表达式来创建该接口的对象。（若 Lambda表达式抛出一个受检异常，那么该异常需要在目标接口的抽象方法上进行声明）。
+    可以在任意函数式接口上使用 @FunctionalInterface 注解，这样做可以检查它是否是一个函数式接口，
+    同时 javadoc 也会包含一条声明，说明这个接口是一个函数式接口    
+*/
 //
 @FunctionalInterface
 interface MyFunc<T> {
